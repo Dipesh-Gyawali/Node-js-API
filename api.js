d977b31f1a3927421cdaac7a1e9655c071dcede5
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-05-07 15:43:49 +0545

    first commit

---
+++ api.js
@@ -0,0 +1,16 @@
+// const dbConnect = require("./mongodb");
+// const express = require("express");
+// const app = express();
+
+// app.use(express.json());
+
+// app.get("/", (req, resp) => {
+//   //   let data = await dbConnect();
+//   //   data = await data.find().toArray();
+//   //   console.log(data);
+//   //   console.warn(data);
+
+//   resp.send({ name: "anil" });
+// });
+
+// app.listen(5173);
